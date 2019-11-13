43257efcc861ad8b5a2d8d345d7baa0269e17ed0
Andrew Crosby <acrosby@redhat.com>
2019-11-13 17:21:51 -0500

    Get openshift script and image working inside minishift
    Signed-off-by: Andrew Crosby <acrosby@redhat.com>

--- docker/ansible-test/entrypoint.sh
+++ docker/ansible-test/entrypoint.sh
@@ -2,18 +2,17 @@
 set -e
 
 # Extract collection to path needed for ansible-test sanity
-mkdir -p /home/user1/ansible_collections/placeholder_namespace/placeholder_name
-cd /home/user1/ansible_collections/placeholder_namespace/placeholder_name
+mkdir -p /ansible_collections/placeholder_namespace/placeholder_name
+cd /ansible_collections/placeholder_namespace/placeholder_name
 
-# cd /collection_volume
-# pwd
-# whoami
-# ls
-# cd /home/user1/ansible_collections/placeholder_namespace/placeholder_name
+# echo "Downloading collection..."
+# curl --insecure --user ansible-insights:redhat https://ci.cloud.redhat.com/api/automation-hub/v3/artifacts/collections/awcrosby.collection_test.1.0.2 -L --output archive.tar.gz
+# echo "Extracting collection archive..."
+# tar -xzf archive.tar.gz
 
-cp /collection_volume/*.tar.gz .
-echo "Extracting collection archive..."
-tar -xzf *.tar.gz
+echo "DEBUG action... Copying/extracting archive instead of downloading..."
+cp /google-gcp-1.0.0.tar.gz .
+tar -xzf google-gcp-1.0.0.tar.gz
 
 # Rename placeholders in path
 NAMESPACE=$(python3 -c "import json; f = open('MANIFEST.json'); namespace = json.load(f)['collection_info']['namespace']; print(namespace); f.close")
@@ -21,9 +20,9 @@ NAME=$(python3 -c "import json; f = open('MANIFEST.json'); name = json.load(f)['
 cd ../../
 mv placeholder_namespace/placeholder_name placeholder_namespace/$NAME
 mv placeholder_namespace/ $NAMESPACE
+cd /ansible_collections/$NAMESPACE/$NAME
 
-cd /home/user1/ansible_collections/$NAMESPACE/$NAME
 echo "Running ansible-test sanity..."
-/venv/bin/ansible-test sanity
+/venv/bin/ansible-test sanity --color
 
 exec "$@"
--- docker/ansible-test/openshift-run.sh
+++ docker/ansible-test/openshift-run.sh
@@ -1,24 +1,33 @@
-# NOTE: below cannot copy entrypoint, think no build context on stdin
-# echo 'Create openshift buildconfig and image...'
+# NOTE: Don't use this block, below cannot copy entrypoint, think no build context on stdin
+# echo 'Create from:Dockerfile buildconfig via Dockerfile'
 # cat Dockerfile | oc new-build -D - --name ansible-test2
 
-# echo 'Create binary buildconfig via Dockerfile'
+# NOTE: Don't use this block, use jobs instead
+# # CrashLoopBackOff likely since no long running project
+# echo 'Runs pod from image under openshift'
+# oc run mytestapp5 --image=172.30.1.1:5000/myproject/mytest
+# oc get deploymentconfig
+
+# echo 'Create from:Binary buildconfig via Dockerfile'
 # oc new-build \
 #     --strategy docker \
 #     --binary \
 #     --docker-image centos:centos7 \
 #     --name bin-ans-test
 # echo 'Start (or redo) binary build using local dir content'
-# oc start-build bin-ans-test --from-dir . --follow
+oc start-build bin-ans-test --from-dir . --follow
 
 # echo 'Get buildconfig and imagesstreams'
 # oc get bc
 # oc get is
+# oc describe bc/bin-ans-test
+# oc edit bc/bin-ans-test
 
+echo 'Creating and running openshift job...'
 echo 'apiVersion: batch/v1
 kind: Job
 metadata:
-  name: bin-ans-test3
+  name: bin-ans-test
 spec:
   parallelism: 1    
   completions: 1    
@@ -34,11 +43,20 @@ spec:
         # command: ["ls"]
       restartPolicy: OnFailure
 ' >> job.yaml
-echo 'Create openshift job'
 oc create -f job.yaml
 rm job.yaml
+
+sleep 5
+
+echo 'Getting logs from job'
+oc logs jobs/bin-ans-test -f
+
+echo 'Deleting files...'
+oc delete job/bin-ans-test
+
+
 
 # # Get pod name and look at log output
 # oc get pods
 # oc logs bin-ans-test-k6lnj
-
+# oc debug pod/mytestapp7-1-z6rk5  # shell into pod
